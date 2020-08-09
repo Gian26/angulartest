a56b667e38fbcf05d768d5c4e77a26aaa792e3d4
unknown <alexchuc84@gmail.com>
2020-08-09 00:47:12 -0500

    agregado funcion para la seleccion de elemntos por pagina

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -13,10 +13,26 @@ export class TableComponent implements OnInit {
     {folio: '2', servicio: '2', serieInicial: '2', serieFinal: '2', cantidad: 30, estado: true},
     {folio: '3', servicio: '3', serieInicial: '3', serieFinal: '3', cantidad: 24, estado: false},
     {folio: '4', servicio: '4', serieInicial: '4', serieFinal: '4', cantidad: 242, estado: true},
+    {folio: '4', servicio: '4', serieInicial: '4', serieFinal: '4', cantidad: 242, estado: true},
+    {folio: '4', servicio: '4', serieInicial: '4', serieFinal: '4', cantidad: 242, estado: true},
   ];
 
+  paginationOptions = [
+    {value: 5, text: '5 elementos'},
+    {value: 10, text: '10 elementos'},
+    {value: 15, text: '15 elementos'},
+  ]
 
+  selectedPagination = this.paginationOptions[0];
+  actualNuberOfElementsInPage: number;
   ngOnInit() {
+    this.actualNuberOfElementsInPage = this.array.length < this.selectedPagination.value ? this.array.length : this.selectedPagination.value;
+  }
+  
+  choosePagination({value: numberOfElements}){
+    
+    console.log(numberOfElements);
+    
   }
 
 }
